137fc2e5c8f1a2eb94dbd50fee3803d2495e26f8
sjgarza22 <sjgarza22@gmail.com>
2020-07-04 19:35:42 -0500

    Create table Posts code added

--- db/migrate/20200701021452_create_posts.rb
+++ db/migrate/20200701021452_create_posts.rb
@@ -1,4 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.datetime :created_at
+      t.integer :user_id
+    end
   end
 end
