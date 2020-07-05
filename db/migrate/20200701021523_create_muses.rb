54f425a7b5ed502cbf52ee10b04b38b8ed60351f
sjgarza22 <sjgarza22@gmail.com>
2020-07-04 19:39:09 -0500

    Create Table Muses code added

--- db/migrate/20200701021523_create_muses.rb
+++ db/migrate/20200701021523_create_muses.rb
@@ -1,4 +1,10 @@
 class CreateMuses < ActiveRecord::Migration
   def change
+    create_table :muses do |t|
+      t.string :name
+      t.text :about
+      t.integer :user_id
+      t.integer :post_id
+    end
   end
 end
