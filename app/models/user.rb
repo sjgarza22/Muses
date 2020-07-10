01074fa0374b0b3c2f9b1cead8c805bdb8271b83
sjgarza22 <sjgarza22@gmail.com>
2020-07-09 21:53:13 -0500

    Fixed has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :posts, :muses
+    has_many :posts
+    has_many :muses
     validates_presence_of :username, :email, :password
 end
