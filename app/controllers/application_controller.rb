dae4174d6f38f0dcfa3f01e735c70be753b99d3d
sjgarza22 <sjgarza22@gmail.com>
2020-07-10 16:31:46 -0500

    Added Get for Dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,16 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ""
   end
 
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  get '/dashboard' do
+    erb :dashboard
   end
 
 end
