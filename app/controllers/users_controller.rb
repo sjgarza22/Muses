7d66a4f1310c1609c81c1030f3541d893e0df294
sjgarza22 <sjgarza22@gmail.com>
2020-07-10 15:46:05 -0500

    Completed User's Get/Post Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+    get '/users/signup' do
+        erb :'/users/signup'
+    end
 
+    post '/users/signup' do
+        user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
+        if user.save
+            redirect '/users/login'
+        else
+            redirect '/users/signup'
+        end
+    end
+
+    get '/users/login' do
+        erb :'/users/login'
+    end
+
+    # post '/users/login' do
+
+    #     # redirect 'index'
+    # end
+
+    # get '/users/:username' do
+    #     erb :'/users/profile'
+    # end
+
+    # get '/users/settings' do
+    #     erb :'/users/settings'
+    # end
 end
