1ef4217295d71ea371a3a000ef3c07c5e500316e
sjgarza22 <sjgarza22@gmail.com>
2020-07-11 20:59:46 -0500

    Set session_secret & add register Sinatra Flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 require './config/environment'
+require 'sinatra/base'
+require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
 
@@ -6,7 +8,8 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, ""
+    set :session_secret, "61b86f939e883bca157a561d"
+    register Sinatra::Flash
   end
 
   get "/" do
