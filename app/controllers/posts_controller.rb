f1cd4dfd54e60f49ba1310f8a4b528a49aff561c
sjgarza22 <sjgarza22@gmail.com>
2020-07-20 23:18:03 -0500

    Add Get for Show & Get/Post for New

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
 
+    get '/posts' do
+        @posts = Post.all
+        erb :'/posts/show'
+    end
+
+    get '/posts/new' do
+        erb :'/posts/new'
+    end
+
+    post '/posts/new' do
+        post = Post.create(title: param(:title), content: param(:content))
+        
+    end
 
 end
