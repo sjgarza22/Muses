2fc60475cfaf2aef61a1ef766417916fb001c4b7
sjgarza22 <sjgarza22@gmail.com>
2020-07-20 23:30:35 -0500

    Changed login/signup route & saved user session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,29 @@
 class UsersController < ApplicationController
-    get '/users/signup' do
+    get '/signup' do
         erb :'/users/signup'
     end
 
-    post '/users/signup' do
+    post '/signup' do
         user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
         if user.save
-            redirect '/users/login'
+            session[:user_id] = user.id
+            redirect '/dashboard'
         else   
             flash.next[:signup_error] = "One or more items in the form were left blank. Please try again."
             redirect '/users/signup'
         end
     end
 
-    get '/users/login' do
+    get '/login' do
         erb :'/users/login'
     end
 
-    post '/users/login' do
+    post '/login' do
         user = User.find_by(username: params[:username])
 
         if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
             redirect '/dashboard'
         else
             flash.next[:login_error] = "One or more items were left blank. Please try again."
