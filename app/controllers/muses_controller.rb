03640d20d2e4c2e8333612840664cae52798c1d5
sjgarza22 <sjgarza22@gmail.com>
2020-07-20 23:17:19 -0500

    Add Get for Show & Get/Post for New

--- app/controllers/muses_controller.rb
+++ app/controllers/muses_controller.rb
@@ -1,4 +1,17 @@
 class MusesController < ApplicationController
 
+    get '/muses' do
+        erb :'/muses/show'
+    end
+
+    get '/muses/new' do
+        erb :'/muses/new'
+    end
+
+    post '/muses/new' do
+        muse = Muse.create(name: params[:name])
+
+        
+    end
 
 end
