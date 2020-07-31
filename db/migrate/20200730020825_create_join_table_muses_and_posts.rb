9eb81381ff35d4cd3616667b196b48913b7bd588
sjgarza22 <sjgarza22@gmail.com>
2020-07-31 11:06:16 -0500

    Added underscore in name btn muse & posts

--- db/migrate/20200730020825_create_join_table_muses_and_posts.rb
+++ db/migrate/20200730020825_create_join_table_muses_and_posts.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableMusesAndPosts < ActiveRecord::Migration
   def change
-    create_table :museposts do |t|
+    create_table :muse_posts do |t|
       t.integer :muse_id
       t.integer :post_id
     end
