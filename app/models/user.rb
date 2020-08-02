6149fcae6ab709373d85f0a670693f9954b381e6
sjgarza22 <sjgarza22@gmail.com>
2020-08-02 16:18:44 -0500

    Add unique validation for email & username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :posts
     has_many :muses
+    validates :email, uniqueness: true
+    validates :username, uniqueness: true
     validates_presence_of :username, :email, :password
 end
